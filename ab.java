c2e5b4e3019d33a0ddbfbd8ba1bb92f206e7c647
podilaanusha <podilaanusha45@ggmail.com>
2024-04-17 10:49:09 +0530

    this is my java

---
+++ ab.java
@@ -0,0 +1,17 @@
+package extend;
+
+public class ab {
+void m1() {
+	System.out.println("hello");
+}
+public static void main(String[] args) {
+}	
+}
+class b extends ab{
+	public static void main(String[] args) {
+		b obj=new b();
+			obj.m1();
+		}
+
+	}
+
